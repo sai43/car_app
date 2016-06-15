601edf7b943ddeda18abfb3a0e9ef9e3b1ca601e
sai ch <chsai.btech@gmail.com>
2016-06-15 17:41:21 +0530

    validate presence make for car model added

--- spec/models/car.rb
+++ spec/models/car.rb
@@ -1,9 +1,23 @@
 require "rails_helper"
 
 RSpec.describe Car, :type => :model do
-  it "cars by make" do
-    benze = Car.create!(make: "Benze", year: "2osix", color: "Black", wheels: "4", doors: "4")
-    # ferrari = Car.create!(make: "Ferrari", year: "2016", color: "Red", wheels: "4", doors: "3")
-    expect(Car.find_by_make("Benze")).to eq(benze)
+
+  # it "cars by make" do
+  #   benze = Car.create!(make: "Benze", year: "2016", color: "Black", wheels: "4", doors: "4")
+  #   expect(Car.find_by_make("Benze")).to eq(benze)
+  # end
+
+  # it "should be failed for invalid datatype" do
+  #    ferrari = Car.create!(make: "Ferrari", year: "2016s", color: "Red", wheels: "4", doors: "3")
+  #    expect(Car.find_by_make("Benze")).to eq(ferrari)
+  # end
+
+  # it { should validate_presence_of :make }
+
+  it "should require a make" do
+    car = Car.create(:make => "")
+    car.valid?
+    car.errors.should have_key(:make)
   end
+
 end
